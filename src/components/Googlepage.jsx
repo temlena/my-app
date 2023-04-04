51b464ac40da7ca3fc0df9079d13cb08d6b4197c
temlena <oridupahelen@gmail.com>
2023-04-04 20:42:53 +0100

    first commit

---
+++ src/components/Googlepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Heading from './Heading';
+import Search from './Search';
+import Footer from './Footer';
+
+const Googlepage = () => {
+    return (
+      <>
+        <Heading />
+        <Search />
+        <Footer />
+      </>
+    );
+  };
+  
+  export default Googlepage;
